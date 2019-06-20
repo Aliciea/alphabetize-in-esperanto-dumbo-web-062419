4e033c6b3c947e34496194400b60cdadccf1f162
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 00:21:32 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,9 +3,11 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
     arr.sort(esperanto_alphabet)
       arr.sort_by do |esperanto_alphabet|
-  esperanto_alphabet.split(" ")
-  esperanto_alphabet = array
-    esperanto_array.index(individual_letters)
+        esperanto_alphabet.split(" ")
+          esperanto_alphabet = array
+           esperanto_array.index(individual_letters)
+         end
+       end
     end
   end   
 end
